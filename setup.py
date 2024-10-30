55a4147210c408703116fc9f42a684404a177c24
DoodleBears <yangmufeng233@gmail.com>
2024-10-30 22:57:27 +0900

    version(v2.0.4): release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*relpath):
 
 setup(
     name="split_lang",
-    version="2.0.3",
+    version="2.0.4",
     description="A package for splitting text by languages through concatenating over split substrings based on their language",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
